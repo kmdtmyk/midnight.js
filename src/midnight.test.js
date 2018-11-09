b1ebfa452bcd74a410b240c76c8a89136a50ecdf
kmdtmyk <winback_banderas@yahoo.co.jp>
2018-11-09 10:52:49 +0900

    Refactoring test code

--- src/midnight.test.js
+++ src/midnight.test.js
@@ -1,5 +1,23 @@
 import Midnight from './midnight'
 
+expect.extend({
+  toSameDate(received, argument){
+    const date1 = new Date(received)
+    const date2 = new Date(argument)
+    const pass = date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate()
+    return {
+      pass,
+      message(){
+        if(pass){
+          return `${received} and ${argument} are same date`
+        }else{
+          return `${received} and ${argument} are not same date`
+        }
+      }
+    }
+  }
+})
+
 test('time is fixed on 00:00:00.000', () => {
   const date = new Midnight()
   expect(date.getHours()).toBe(0)
@@ -14,9 +32,9 @@ test('instanceof Date', () => {
 })
 
 test('create', () => {
-  expect(Midnight.equals(Midnight.create(2017, 5, 20), '2017/05/20')).toBe(true)
-  expect(Midnight.equals(Midnight.create(2017, 5, 0), '2017/04/30')).toBe(true)
-  expect(Midnight.equals(Midnight.create(2017, 5, 32), '2017/06/01')).toBe(true)
+  expect(Midnight.create(2017, 5, 20)).toSameDate('2017/05/20')
+  expect(Midnight.create(2017, 5, 0)).toSameDate('2017/04/30')
+  expect(Midnight.create(2017, 5, 32)).toSameDate('2017/06/01')
 })
 
 test('differenceInDays', () => {
@@ -33,19 +51,19 @@ test('differenceInDays', () => {
 })
 
 test('endOfMonth', () => {
-  expect(Midnight.equals(new Midnight('2016/08/01').endOfMonth(), '2016/08/31')).toBe(true)
-  expect(Midnight.equals(new Midnight('2016/08/15').endOfMonth(), '2016/08/31')).toBe(true)
-  expect(Midnight.equals(new Midnight('2016/08/31').endOfMonth(), '2016/08/31')).toBe(true)
+  expect(new Midnight('2016/08/01').endOfMonth()).toSameDate('2016/08/31')
+  expect(new Midnight('2016/08/15').endOfMonth()).toSameDate('2016/08/31')
+  expect(new Midnight('2016/08/31').endOfMonth()).toSameDate('2016/08/31')
 
-  expect(Midnight.equals(new Midnight('2016/02/25').endOfMonth(), '2016/02/29')).toBe(true)
-  expect(Midnight.equals(new Midnight('2017/02/25').endOfMonth(), '2017/02/28')).toBe(true)
+  expect(new Midnight('2016/02/25').endOfMonth()).toSameDate('2016/02/29')
+  expect(new Midnight('2017/02/25').endOfMonth()).toSameDate('2017/02/28')
 })
 
 test('endOfYear', () => {
-  expect(Midnight.equals(new Midnight('2017/01/01').endOfYear(), '2017/12/31')).toBe(true)
-  expect(Midnight.equals(new Midnight('2017/05/15').endOfYear(), '2017/12/31')).toBe(true)
-  expect(Midnight.equals(new Midnight('2017/12/31').endOfYear(), '2017/12/31')).toBe(true)
-  expect(Midnight.equals(new Midnight('2018/01/01').endOfYear(), '2018/12/31')).toBe(true)
+  expect(new Midnight('2017/01/01').endOfYear()).toSameDate('2017/12/31')
+  expect(new Midnight('2017/05/15').endOfYear()).toSameDate('2017/12/31')
+  expect(new Midnight('2017/12/01').endOfYear()).toSameDate('2017/12/31')
+  expect(new Midnight('2018/01/31').endOfYear()).toSameDate('2018/12/31')
 })
 
 test('equals', () => {
@@ -72,63 +90,67 @@ test('static equals', () => {
 test('day', () => {
   const date = new Midnight('2017/05/15')
   expect(date.day()).toBe(15)
-  expect(Midnight.equals(date.day(0), '2017/04/30')).toBe(true)
-  expect(Midnight.equals(date.day(1), '2017/05/01')).toBe(true)
-  expect(Midnight.equals(date.day(31), '2017/05/31')).toBe(true)
-  expect(Midnight.equals(date.day(32), '2017/06/01')).toBe(true)
+  expect(date.day(0)).toSameDate('2017/04/30')
+  expect(date.day(1)).toSameDate('2017/05/01')
+  expect(date.day(31)).toSameDate('2017/05/31')
+  expect(date.day(32)).toSameDate('2017/06/01')
 })
 
 test('month', () => {
   let date = new Midnight('2017/05/15')
   expect(date.month()).toBe(5)
-  expect(Midnight.equals(date.month(4), '2017/04/15')).toBe(true)
-  expect(Midnight.equals(date.month(6), '2017/06/15')).toBe(true)
+  expect(date.month(-1)).toSameDate('2016/11/15')
+  expect(date.month(0)).toSameDate('2016/12/15')
+  expect(date.month(4)).toSameDate('2017/04/15')
+  expect(date.month(6)).toSameDate('2017/06/15')
+  expect(date.month(12)).toSameDate('2017/12/15')
+  expect(date.month(13)).toSameDate('2018/01/15')
 
   date = new Midnight('2016/08/31')
-  expect(Midnight.equals(date.month(9), '2016/09/30')).toBe(true)
-  expect(Midnight.equals(date.month(2), '2016/02/29')).toBe(true)
-  expect(Midnight.equals(date.month(6), '2016/06/30')).toBe(true)
+  expect(date.month(9)).toSameDate('2016/09/30')
+  expect(date.month(2)).toSameDate('2016/02/29')
+  expect(date.month(6)).toSameDate('2016/06/30')
 })
 
 test('nextDay', () => {
   const date = new Midnight('2017/05/15')
-  expect(Midnight.equals(date.nextDay(), '2017/05/16')).toBe(true)
-  expect(Midnight.equals(date.nextDay(-15), '2017/04/30')).toBe(true)
-  expect(Midnight.equals(date.nextDay(-1), '2017/05/14')).toBe(true)
-  expect(Midnight.equals(date.nextDay(0), '2017/05/15')).toBe(true)
-  expect(Midnight.equals(date.nextDay(1), '2017/05/16')).toBe(true)
-  expect(Midnight.equals(date.nextDay(17), '2017/06/01')).toBe(true)
+  expect(date.nextDay()).toSameDate('2017/05/16')
+  expect(date.nextDay(-15)).toSameDate('2017/04/30')
+  expect(date.nextDay(-1)).toSameDate('2017/05/14')
+  expect(date.nextDay(0)).toSameDate('2017/05/15')
+  expect(date.nextDay(1)).toSameDate('2017/05/16')
+  expect(date.nextDay(17)).toSameDate('2017/06/01')
 })
 
 test('nextMonth', () => {
   let date = new Midnight('2016/08/01')
-  expect(Midnight.equals(date.nextMonth(), '2016/09/01')).toBe(true)
-  expect(Midnight.equals(date.nextMonth(1), '2016/09/01')).toBe(true)
-  expect(Midnight.equals(date.nextMonth(4), '2016/12/01')).toBe(true)
-  expect(Midnight.equals(date.nextMonth(5), '2017/01/01')).toBe(true)
+  expect(date.nextMonth()).toSameDate('2016/09/01')
+  expect(date.nextMonth(1)).toSameDate('2016/09/01')
+  expect(date.nextMonth(4)).toSameDate('2016/12/01')
+  expect(date.nextMonth(5)).toSameDate('2017/01/01')
 
   date = new Midnight('2016/08/15')
-  expect(Midnight.equals(date.nextMonth(-1), '2016/07/15')).toBe(true)
-  expect(Midnight.equals(date.nextMonth(0), '2016/08/15')).toBe(true)
-  expect(Midnight.equals(date.nextMonth(1), '2016/09/15')).toBe(true)
+  expect(date.nextMonth(-1)).toSameDate('2016/07/15')
+  expect(date.nextMonth(0)).toSameDate('2016/08/15')
+  expect(date.nextMonth(1)).toSameDate('2016/09/15')
 
   date = new Midnight('2016/08/31')
-  expect(Midnight.equals(date.nextMonth(1), '2016/09/30')).toBe(true)
-  expect(Midnight.equals(date.nextMonth(6), '2017/02/28')).toBe(true)
-  expect(Midnight.equals(date.nextMonth(-2), '2016/06/30')).toBe(true)
+  expect(date.nextMonth(1)).toSameDate('2016/09/30')
+  expect(date.nextMonth(6)).toSameDate('2017/02/28')
+  expect(date.nextMonth(-2)).toSameDate('2016/06/30')
 })
 
 test('nextYear', () => {
   let date = new Midnight('2016/05/15')
-  expect(Midnight.equals(date.nextYear(), '2017/05/15')).toBe(true)
-  expect(Midnight.equals(date.nextYear(-1), '2015/05/15')).toBe(true)
-  expect(Midnight.equals(date.nextYear(0), '2016/05/15')).toBe(true)
-  expect(Midnight.equals(date.nextYear(1), '2017/05/15')).toBe(true)
+  expect(date.nextYear()).toSameDate('2017/05/15')
+  expect(date.nextYear(-1)).toSameDate('2015/05/15')
+  expect(date.nextYear(0)).toSameDate('2016/05/15')
+  expect(date.nextYear(1)).toSameDate('2017/05/15')
 
   date = new Midnight('2016/02/29')
-  expect(Midnight.equals(date.nextYear(-1), '2015/02/28')).toBe(true)
-  expect(Midnight.equals(date.nextYear(1), '2017/02/28')).toBe(true)
-  expect(Midnight.equals(date.nextYear(4), '2020/02/29')).toBe(true)
+  expect(date.nextYear(-1)).toSameDate('2015/02/28')
+  expect(date.nextYear(1)).toSameDate('2017/02/28')
+  expect(date.nextYear(4)).toSameDate('2020/02/29')
 })
 
 test('setHours', () => {
@@ -171,16 +193,15 @@ test('setTime', () => {
 })
 
 test('startOfMonth', () => {
-  expect(Midnight.equals(new Midnight('2017/05/01').startOfMonth(), '2017/05/01')).toBe(true)
-  expect(Midnight.equals(new Midnight('2017/05/20').startOfMonth(), '2017/05/01')).toBe(true)
-  expect(Midnight.equals(new Midnight('2017/05/31').startOfMonth(), '2017/05/01')).toBe(true)
+  expect(new Midnight('2017/05/01').startOfMonth()).toSameDate('2017/05/01')
+  expect(new Midnight('2017/05/15').startOfMonth()).toSameDate('2017/05/01')
+  expect(new Midnight('2017/05/31').startOfMonth()).toSameDate('2017/05/01')
 })
 
 test('startOfYear', () => {
-  expect(Midnight.equals(new Midnight('2017/01/01').startOfYear(), '2017/01/01')).toBe(true)
-  expect(Midnight.equals(new Midnight('2017/05/15').startOfYear(), '2017/01/01')).toBe(true)
-  expect(Midnight.equals(new Midnight('2017/12/31').startOfYear(), '2017/01/01')).toBe(true)
-  expect(Midnight.equals(new Midnight('2018/01/01').startOfYear(), '2018/01/01')).toBe(true)
+  expect(new Midnight('2017/01/01').startOfYear()).toSameDate('2017/01/01')
+  expect(new Midnight('2017/05/15').startOfYear()).toSameDate('2017/01/01')
+  expect(new Midnight('2017/12/31').startOfYear()).toSameDate('2017/01/01')
 })
 
 test('toObject', () => {
@@ -191,34 +212,34 @@ test('toObject', () => {
 test('year', () => {
   let date = new Midnight('2017/05/20')
   expect(date.year()).toBe(2017)
-  expect(Midnight.equals(date.year(2016), '2016/05/20')).toBe(true)
-  expect(Midnight.equals(date.year(2018), '2018/05/20')).toBe(true)
+  expect(date.year(2016)).toSameDate('2016/05/20')
+  expect(date.year(2018)).toSameDate('2018/05/20')
 
   date = new Midnight('2016/02/29')
-  expect(Midnight.equals(date.year(2017), '2017/02/28')).toBe(true)
+  expect(date.year(2017)).toSameDate('2017/02/28')
 })
 
 test('immutable', () => {
   const value = '2017/05/20'
   const date = new Midnight(value)
   date.day(1)
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.month(1)
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.year(2000)
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.nextDay(1)
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.nextMonth(1)
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.nextYear(1)
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.startOfMonth()
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.startOfYear()
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.endOfMonth()
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
   date.endOfYear()
-  expect(Midnight.equals(date, value)).toBe(true)
+  expect(date).toSameDate(value)
 })
